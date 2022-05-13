7ef805881eb2810e3385f971034eb0b873f4f611
Henrique <49873860+henriquesdm7@users.noreply.github.com>
2022-05-13 18:01:34 -0300

    feat: tabela verdade

---
+++ tabela_verdade.py
@@ -0,0 +1,30 @@
+def tabelaVerdade(expr):
+    expr_name = expr[0]
+    x_y = [
+        (True, True),
+        (True, False),
+        (False, True),
+        (False, False),
+    ]
+
+    col_1 = 15
+    col_2 = 15
+    col_3 = 20
+    
+    print(f"╔{'═'*(col_1)}╦{'═'*(col_2)}╦{'═'*(col_3)}╗")
+    print(f"║{'x':^{col_1}}║{'y':^{col_2}}║{'x '+expr_name+' y':^{col_3}}║")
+    print(f"╠{'═'*(col_1)}╬{'═'*(col_2)}╬{'═'*(col_3)}╣")
+    for x, y in x_y:
+       
+        print(f"║{str(x):^{col_1}}║{str(y):^{col_2}}║{str(expr[1](x,y)):^{col_3}}║")
+        # print(f"X: {x} e Y: {y}")
+    print(f"╚{'═'*(col_1)}╩{'═'*(col_2)}╩{'═'*(col_3)}╝")
+
+f_and   = ['AND', (lambda x, y:     (x and y))]
+f_nand  = ['NAND',(lambda x, y: not (x and y))]
+f_or    = ['OR',  (lambda x, y:     (x or y))]
+f_nor   = ['NOR', (lambda x, y: not (x or y))]
+f_xor   = ['XOR', (lambda x, y:     ((x or y) and ((not x) or (not y))))]
+f_xnor  = ['XNOR',(lambda x, y: not ((x or y) and ((not x) or (not y))))]
+
+tabelaVerdade(f_nand)
