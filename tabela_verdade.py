dd7165584c28bf1198e6d84696befff4d8576153
Henrique <49873860+henriquesdm7@users.noreply.github.com>
2022-05-16 08:11:00 -0300

    Update tabela_verdade.py

--- tabela_verdade.py
+++ tabela_verdade.py
@@ -15,9 +15,7 @@ def tabelaVerdade(expr):
     print(f"║{'x':^{col_1}}║{'y':^{col_2}}║{'x '+expr_name+' y':^{col_3}}║")
     print(f"╠{'═'*(col_1)}╬{'═'*(col_2)}╬{'═'*(col_3)}╣")
     for x, y in x_y:
-       
         print(f"║{str(x):^{col_1}}║{str(y):^{col_2}}║{str(expr[1](x,y)):^{col_3}}║")
-        # print(f"X: {x} e Y: {y}")
     print(f"╚{'═'*(col_1)}╩{'═'*(col_2)}╩{'═'*(col_3)}╝")
 
 f_and   = ['AND', (lambda x, y:     (x and y))]
@@ -27,4 +25,9 @@ f_nor   = ['NOR', (lambda x, y: not (x or y))]
 f_xor   = ['XOR', (lambda x, y:     ((x or y) and ((not x) or (not y))))]
 f_xnor  = ['XNOR',(lambda x, y: not ((x or y) and ((not x) or (not y))))]
 
+tabelaVerdade(f_and)
 tabelaVerdade(f_nand)
+tabelaVerdade(f_or)
+tabelaVerdade(f_nor)
+tabelaVerdade(f_xor)
+tabelaVerdade(f_xnor)
